f31506f1669e39aa88c4187184b9bf4b75d731a3
lightedblue <lightedblue@gmail.com>
2017-01-23 00:13:56 +0900

    First

---
+++ jenkinstest/src/main/java/jp/lightedblue/ServletInitializer.java
@@ -0,0 +1,13 @@
+package jp.lightedblue;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinstestApplication.class);
+	}
+
+}
---
+++ jenkinstest/src/test/java/jp/lightedblue/JenkinstestApplicationTests.java
@@ -0,0 +1,33 @@
+package jp.lightedblue;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinstestApplicationTests {
+
+	private MockMvc mvc;
+
+    @Before
+    public void before() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void testGet__Ok() throws Exception {
+        mvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Hello Spring MVC"));
+    }
+
+}
